3dfa2d4044180d4976ce487d3e4c1ea868a0678d
Nechai_Ihor <fredduke1983@gmail.com>
2023-07-25 22:26:41 +0300

    FIX: fixed pattern pass for register

--- utils/userValidates.js
+++ utils/userValidates.js
@@ -10,7 +10,7 @@ exports.userSignupValidator = (data) =>
         .pattern(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)
         .required(),
       password: Joi.string()
-        .pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$/) //* pass must include at least one upper case letter, one lower case letter, and one numeric digit. Must be at least 4 characters, no more than 8 characters
+        .pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,20}$/) //* pass must include at least one upper case letter, one lower case letter, and one numeric digit. Must be at least 4 characters, no more than 20 characters
         .required(),
     })
     .validate(data);
