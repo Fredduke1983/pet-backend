116900cbacc768f74b73a8e5b3aa521f203117c8
Nechai_Ihor <fredduke1983@gmail.com>
2023-07-26 01:21:55 +0300

    ADD: added routes swager + fixed validate user

--- utils/userValidates.js
+++ utils/userValidates.js
@@ -6,7 +6,6 @@ exports.userSignupValidator = (data) =>
     .keys({
       name: Joi.string().min(3).max(20).required(),
       email: Joi.string()
-        // eslint-disable-next-line no-useless-escape
         .pattern(/^\w+@[a-zA-Z\d-]+\.[a-zA-Z]{2,4}$/)
         .required(),
       password: Joi.string()
@@ -20,11 +19,10 @@ exports.userSigninValidator = (data) =>
     .options({ abortEarly: false })
     .keys({
       email: Joi.string()
-        // eslint-disable-next-line no-useless-escape
-        .pattern(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)
+        .pattern(/^\w+@[a-zA-Z\d-]+\.[a-zA-Z]{2,4}$/)
         .required(),
       password: Joi.string()
-        .pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,20}$/) //* pass must include at least one upper case letter, one lower case letter, and one numeric digit. Must be at least 4 characters, no more than 8 characters
+        .pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,20}$/) //* pass must include at least one upper case letter, one lower case letter, and one numeric digit. Must be at least 4 characters, no more than 20 characters
         .required(),
     })
     .validate(data);
