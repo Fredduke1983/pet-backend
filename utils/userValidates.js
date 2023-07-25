9f30b4c690149a1708685032190bd98b3ccdc9b2
Nechai_Ihor <fredduke1983@gmail.com>
2023-07-25 22:52:35 +0300

    FIX: pass for signin signup

--- utils/userValidates.js
+++ utils/userValidates.js
@@ -7,7 +7,7 @@ exports.userSignupValidator = (data) =>
       name: Joi.string().min(3).max(20).required(),
       email: Joi.string()
         // eslint-disable-next-line no-useless-escape
-        .pattern(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)
+        .pattern(/^\w+@[a-zA-Z\d-]+\.[a-zA-Z]{2,4}$/)
         .required(),
       password: Joi.string()
         .pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,20}$/) //* pass must include at least one upper case letter, one lower case letter, and one numeric digit. Must be at least 4 characters, no more than 20 characters
@@ -24,7 +24,7 @@ exports.userSigninValidator = (data) =>
         .pattern(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)
         .required(),
       password: Joi.string()
-        .pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$/) //* pass must include at least one upper case letter, one lower case letter, and one numeric digit. Must be at least 4 characters, no more than 8 characters
+        .pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,20}$/) //* pass must include at least one upper case letter, one lower case letter, and one numeric digit. Must be at least 4 characters, no more than 8 characters
         .required(),
     })
     .validate(data);
