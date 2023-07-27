f3a689b0854334d56b9be81285eb4400faa50034
Nechai_Ihor <fredduke1983@gmail.com>
2023-07-27 23:10:07 +0300

    ADD: news + swagger

--- controllers/news/getAllNews.js
+++ controllers/news/getAllNews.js
@@ -1,40 +1,26 @@
 const HttpError = require("../../utils/HttpError");
 const News = require("../../models/newsSchema");
+const { ctrlWrapper } = require("../../utils");
 
-const getAllNews = async (req, res) => {
-  const { word } = req.query;
+const getAllNews = ctrlWrapper(async (req, res) => {
+  const { word = "", page = 1, limit = 6 } = req.query;
 
-  const { page = 1, limit = 6 } = req.query;
   const skip = (page - 1) * limit;
 
   try {
     const query = {};
 
-    if (word) {
-      query.title = { $regex: word, $options: "i" };
+    query.title = { $regex: word, $options: "i" };
 
-      const news = await News.find(query).skip(skip).limit(limit);
+    const news = await News.find(query).skip(skip).limit(limit);
 
-      if (news.length === 0) {
-        return res.status(404).json({ error: "News not found" });
-      }
-      res.status(200).json(news);
+    if (news.length === 0) {
+      return res.status(404).json({ error: "News not found" });
     }
+    res.status(200).json(news);
   } catch (error) {
     throw new HttpError(500, "Server failed");
   }
-
-  // const news = await News.find(word).skip(skip).limit(limit);
-
-  // if (news.length === 0) {
-  //   return res.status(404).json({ error: "Notices not found" });
-  // }
-
-  // if (!news) {
-  //   throw HttpError(404);
-  // }
-
-  // res.status(200).json(news);
-};
+});
 
 module.exports = getAllNews;
