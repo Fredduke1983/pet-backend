1a3781836201af4b5b00979c7342bb5549802ea5
Vitaly Nelyub <nelyub86@ukr.net>
2023-07-27 22:44:34 +0300

    add logic for search news

--- controllers/news/getAllNews.js
+++ controllers/news/getAllNews.js
@@ -2,13 +2,39 @@ const HttpError = require("../../utils/HttpError");
 const News = require("../../models/newsSchema");
 
 const getAllNews = async (req, res) => {
-  const news = await News.find();
+  const { word } = req.query;
 
-  if (!news) {
-    throw HttpError(404);
+  const { page = 1, limit = 6 } = req.query;
+  const skip = (page - 1) * limit;
+
+  try {
+    const query = {};
+
+    if (word) {
+      query.title = { $regex: word, $options: "i" };
+
+      const news = await News.find(query).skip(skip).limit(limit);
+
+      if (news.length === 0) {
+        return res.status(404).json({ error: "News not found" });
+      }
+      res.status(200).json(news);
+    }
+  } catch (error) {
+    throw new HttpError(500, "Server failed");
   }
 
-  res.status(200).json(news);
+  // const news = await News.find(word).skip(skip).limit(limit);
+
+  // if (news.length === 0) {
+  //   return res.status(404).json({ error: "Notices not found" });
+  // }
+
+  // if (!news) {
+  //   throw HttpError(404);
+  // }
+
+  // res.status(200).json(news);
 };
 
 module.exports = getAllNews;
